e02db47dc625cc0b56eade3bdd0e0e1aae3db91a
uttamthegoat <19516uttam@gmail.com>
2024-05-29 20:57:27 +0530

    completed saving user details in database

--- lib/state/user_provider.dart
+++ lib/state/user_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -17,24 +19,6 @@ class UserProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> signInWithGoogle() async {
-    try {
-      GoogleAuthProvider googleProvider = GoogleAuthProvider();
-      await _auth.signInWithProvider(googleProvider);
-
-      // Ensure the user's authentication state is updated
-      await _auth.currentUser!.reload();
-
-      if (_auth.currentUser?.emailVerified ?? false) {
-        notifyListeners();
-      } else {
-        print('User email is not verified.');
-        // Optionally handle the case where the email is not verified
-      }
-    } catch (error) {
-      print(error);
-    }
-  }
 
   Future<void> signOut() async {
     await _auth.signOut();
@@ -57,6 +41,23 @@ class UserProvider with ChangeNotifier {
       _onAuthStateChanged(userCredential.user);
 
       if (_user?.emailVerified ?? false) {
+        // database code
+        if(!(await emailExists(_user?.email))){
+
+        final newUser = <String, dynamic>{
+          "name": _user?.displayName,
+          "email": _user?.email,
+          "imageUrl": _user?.photoURL,
+          "bio": "",
+          "username": _user != null
+              ? await generateUsername(_user?.displayName)
+              : _user?.email,
+        };
+        final db = FirebaseFirestore.instance;
+        db.collection("users").add(newUser).then((DocumentReference doc) =>
+            print('DocumentSnapshot added with ID: ${doc.id}'));
+        }
+
         notifyListeners();
       } else {
         print('User email is not verified.');
@@ -66,5 +67,46 @@ class UserProvider with ChangeNotifier {
       print("error");
       // toast a message saying error
     }
+  }
+
+  Future<String> generateUsername(String? name) async {
+    if (name != null) {
+      String sanitized = name.replaceAll(RegExp(r'[^a-zA-Z0-9_]'), '');
+      if (sanitized.isEmpty) {
+        throw ArgumentError(
+            "Name must contain at least one alphanumeric character or underscore.");
+      }
+
+      // Function to check if username exists in Firestore
+      Future<bool> usernameExists(String username) async {
+        final snapshot = await FirebaseFirestore.instance
+            .collection('users')
+            .where('username', isEqualTo: username)
+            .get();
+        return snapshot.docs.isNotEmpty;
+      }
+
+      String username;
+      bool exists;
+
+      do {
+        int randomNumber =
+            Random().nextInt(10000); // Generates a number between 0 and 9999
+        username = "${sanitized}_$randomNumber";
+        exists = await usernameExists(username);
+      } while (exists);
+
+      return username;
+    }
+    return "";
+  }
+
+  Future<bool> emailExists(String? email) async {
+    if (email == null) return false;
+    final snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isEqualTo: email)
+        .get();
+    return snapshot.docs.isNotEmpty;
   }
 }
